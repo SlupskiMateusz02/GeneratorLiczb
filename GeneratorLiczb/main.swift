3830fa5486902dfaee046e16e77a8de1d3ca1445
SlupskiMateusz02 <SlupskiMateusz02@gmail.com>
2025-12-17 22:43:56 +0100

    Fianlna wersja programu

--- GeneratorLiczb/main.swift
+++ GeneratorLiczb/main.swift
@@ -1,11 +1,65 @@
-//
-//  main.swift
-//  GeneratorLiczb
-//
-//  Created by Mateusz Słupski on 17/12/2025.
-//
-
 import Foundation
 
-print("Hello, World!")
+// wczytanie danych
+func wczytajDane() -> String
+{
+    if let input = readLine(), !input.isEmpty
+    {
+        return input
+    }
+    else{
+        print("Podano błędne dane, spróbuj jeszcze raz: ", terminator: "")
+        return wczytajDane()
+    }
+}
+
+// generowanie liczby losowej
+func wygenerujLiczbe() -> Int
+{
+    let liczbaLosowa = Int.random(in: 0...100)
+    return liczbaLosowa
+}
+
+// funkcja główna - odgadywanie liczby przez użytkownika
+func zgadnijLiczbe()
+{
+    print("Podaj ilość prób w których chcesz odgadnąć liczbę: ", terminator: "")
+    let liczbaProb: Int = Int(wczytajDane()) ?? 0
+    var wykorzystaneProby = 0
+    let wylosowanaLiczba = wygenerujLiczbe()
+    
+    // główna pętla programu
+    while liczbaProb > wykorzystaneProby
+    {
+        print("Pozostało prób: \(liczbaProb - wykorzystaneProby)")
+        print("Podaj liczbę: ", terminator: "")
+        let input = wczytajDane()
+        
+        // walidacja żeby stała przyjmowała tylko Inty
+        guard let guess = Int(input), guess > 0 else
+        {
+            print("Podano błędne dane!")
+            return
+        }
+        
+        // sprwadzenie typu użytkownika
+        if guess == wylosowanaLiczba
+        {
+            print("Gratulacje udało ci się zgadnąć za \(wykorzystaneProby + 1) razem. \n")
+            break
+        }
+        else if guess > wylosowanaLiczba
+        {
+            print("Celuj niżej! \n")
+        }
+        else if guess < wylosowanaLiczba
+        {
+            print("Celuj wyżej! \n")
+        }
+        wykorzystaneProby += 1
+    }
+}
+
+// wykonanie programu
+zgadnijLiczbe()
 
